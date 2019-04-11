fea33f8be1f3cbab5b636a5fe6f474932f85c26b
AgustinRamiroDiaz <49416765+AgustinRamiroDiaz@users.noreply.github.com>
2019-04-11 16:22:35 -0300

    Rodri chupame la pija

--- main.c
+++ main.c
@@ -13,15 +13,15 @@ int main(){
 	int cant_paises = carga_archivos("paises.txt", &Paises, size);
 
 
-	printf("???\n" );
-
-
 	if (cant_nombres == 0 || cant_paises == 0) {
 		printf("Error al leer los archivos de nombres y paises. Alguno o ambos no existe o esta vacio\n");
 		return 0;
 	}
 
 	generacion_archivo_personas("salida.txt", Paises, Nombres, cant_nombres, cant_paises, 2000);
+
+	liberar_lista(Nombres, cant_paises);
+	liberar_lista(Paises, cant_paises);
 
 	GList lista_personas = crear_lista();
 	lista_personas = generar_Glista_desde_archivo("salida.txt", lista_personas);
--- manejo_archivos.c
+++ manejo_archivos.c
@@ -77,3 +77,8 @@ void generacion_archivo_personas(char* nombre_salida, char** paises, char** nomb
 
 	fclose(archivo);
 }
+
+void liberar_lista(char** lista, int cantidad_lineas){
+  for(int i = 0; i < cant_lineas, i++)  free(lista[i]);
+  free lista;
+}
