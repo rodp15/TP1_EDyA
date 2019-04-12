6aeb8bca676c234d133b7b1a84437c6e5ff198b9
AgustinRamiroDiaz <agustin.ramiro.diaz@gmail.com>
2019-04-11 22:42:27 -0300

    Comente lista.c

--- lista.c
+++ lista.c
@@ -1,15 +1,20 @@
 #include "main.h"
 
 void* copiar_persona(void* dato){
+	//Casteamos el dato a una Persona para manejarlo mas facil.
 	Persona persona = (Persona)dato;
 
+	//Si la persona es NULL, retornamos NULL.
 	if(!persona) return NULL;
 
+	//Reservamos memoria y copiamos la informacion de la persona original en persona_copiada.
 	Persona persona_copiada = malloc(sizeof(_Persona));
 	memcpy(persona_copiada, persona, sizeof(_Persona));
+
 	int largo_nombre = (strlen(persona->Nombre) + 1),
 		largo_pais =  (strlen(persona->Pais) + 1);
 
+	//Reservamos memoria y copiamos la informacion del Nombre, Edad y Pais.
 	persona_copiada->Nombre = malloc(sizeof(char) * largo_nombre);
 	persona_copiada->Pais = malloc(sizeof(char) * largo_pais);
 	memcpy( persona_copiada->Nombre , persona->Nombre , largo_nombre);
@@ -28,19 +33,23 @@ int es_vacia(GList lista){
 
 void imprimir_lista(GList lista){
 	if (es_vacia(lista)) printf("Lista vacia\n");
+
 	else{
 		GList referencia = lista;
 		for(;lista->sig != referencia; lista = lista->sig){
 			printf("%s, %d, %s\n", ((Persona)lista->dato)->Nombre, ((Persona)lista->dato)->Edad, ((Persona)lista->dato)->Pais);
 		}
+		//Imprimimos el ultimo elemento de la lista.
 		printf("%s, %d, %s\n", ((Persona)lista->dato)->Nombre, ((Persona)lista->dato)->Edad, ((Persona)lista->dato)->Pais);
 	}
 }
 
 Persona crear_persona(char* Nombre, int Edad, char* Pais){
+	//Reservamos memoria para la nueva_persona y su informacion.
 	Persona nueva_persona = malloc(sizeof(_Persona));
 	nueva_persona->Nombre = malloc(sizeof(char) * (strlen(Nombre) + 1));
 	nueva_persona->Pais = malloc(sizeof(char) * (strlen(Pais) + 1));
+	//Guardamos la informacion en la nueva_persona.
 	strcpy(nueva_persona->Nombre, Nombre);
 	nueva_persona->Edad = Edad;
 	strcpy(nueva_persona->Pais, Pais);
@@ -50,17 +59,22 @@ Persona crear_persona(char* Nombre, int Edad, char* Pais){
 
 GList crear_nodo(void* dato){
 	GList nuevo_nodo = malloc(sizeof(GNodo));
+
 	nuevo_nodo->dato = dato;
 	nuevo_nodo->ant = nuevo_nodo;
 	nuevo_nodo->sig = nuevo_nodo;
+
 	return nuevo_nodo;
 }
 
 GList agregar_nodo(GList lista, void* dato){
+	//Creamos el nuevo_nodo que agregaremos al final de la lista.
 	GList nuevo_nodo = crear_nodo(dato);
 
 	if(es_vacia(lista)) return nuevo_nodo;
 
+	//Reasingamos los punteros de la lista y del nuevo_nodo
+	//para que quede el nuevo_nodo como el ultimo de la lista.
 	nuevo_nodo->sig = lista;
 	nuevo_nodo->ant = lista->ant;
 	lista->ant->sig = nuevo_nodo;
@@ -72,14 +86,20 @@ GList agregar_nodo(GList lista, void* dato){
 GList generar_Glista_desde_archivo(char* nombre_archivo, GList lista){
 	FILE *archivo = fopen(nombre_archivo,"r");
 
-	if(archivo == NULL) return lista;
+	if(!archivo) return lista;
 
 	char buff_nom[100], buff_pais[100];
 	int buff_edad;
+
+	//Recorremos el archivo leyendo las lineas hasta el EOF.
+	//Parseamos las lineas para guardar solo la informacion necesaria
+	//y luego agregamos un nodo a la lista con la informacion leida.
 	while(EOF != fscanf(archivo, " %[^,], %d, %[^\r\n]\r\n", buff_nom, &buff_edad, buff_pais)){
 		lista = agregar_nodo(lista, crear_persona(buff_nom, buff_edad, buff_pais));
 	}
+
 	fclose(archivo);
+
 	return lista;
 }
 
@@ -95,7 +115,7 @@ void gList_destruir (GList lista , Destruir d){
 
 	GList i = lista, aux;
 
-	for(;i->sig != lista; i = aux){
+	for(; i->sig != lista; i = aux){
 		d(i->dato);
 		aux = i->sig;
 		free(i);
